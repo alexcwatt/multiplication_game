4fa39e5ceb0f572be30817d92bf16e8f792b108e
Alex Watt <alex@alexcwatt.com>
2022-01-21 08:09:49 -0500

    Basic version of game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Multiplication Game");
+    println!("-------------------");
+    println!("What is the smallest factor you'd like to practice with?");
+    let min = get_integer();
+    println!("And the largest?");
+    let max = get_integer();
+    let facts = build_facts_vector(min, max);
+
+    for fact in facts {
+        println!("{} x {}", fact.0, fact.1);
+        let answer = fact.0 * fact.1;
+        let guess = get_integer();
+
+        match guess.cmp(&answer) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("Perfect!"),
+        }
+    }
+}
+
+fn get_integer() -> i32 {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let number: i32 = input.trim().parse().expect("Please type a number!");
+
+    return number;
+}
+
+fn build_facts_vector(min: i32, max: i32) -> Vec<(i32, i32)> {
+    let mut facts: Vec<(i32, i32)> = Vec::new();
+
+    for i in min..=max {
+        for j in i..=max {
+            facts.push((i, j));
+        }
+    }
+
+    return facts;
 }
